54da40449dbb1d16bd46e6ebfcff655cec52c315
Sudono Tanjung <sudtanj.business@gmail.com>
2020-05-09 11:13:47 +0700

    fix npmjs

--- src/middleware/ErrorHandler.ts
+++ src/middleware/ErrorHandler.ts
@@ -4,7 +4,6 @@ import {
   UnauthorizedError
 } from "routing-controllers";
 import { error } from "../util/FormatUtil";
-import { Constant } from "../constant/Constant";
 
 @Middleware({ type: "after" })
 export class ErrorHandler implements ExpressErrorMiddlewareInterface {
@@ -14,15 +13,12 @@ export class ErrorHandler implements ExpressErrorMiddlewareInterface {
     }
     if (err.constructor === UnauthorizedError) {
       return res
-        .status(Constant.HTTP_CODE.INVALID_CREDENTIAL)
-        .json(error(err.message, Constant.HTTP_CODE.INVALID_CREDENTIAL));
+        .json(error(err.message));
     }
     if (err.constructor === SyntaxError)
       return res
-        .status(Constant.HTTP_CODE.INTERNAL_SERVER_ERROR)
         .json(error("Wrong json!"));
     return res
-      .status(Constant.HTTP_CODE.INTERNAL_SERVER_ERROR)
       .json(error("System error!"));
   }
 }
